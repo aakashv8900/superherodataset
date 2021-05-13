73be488965b9b6b14db9df23e6ba9a38ccc8bde1
Aakash Verma <aakashv.8292@gmail.com>
2021-05-13 13:00:07 +0530

    development

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv, requests, json
 
 with open('data.txt', 'w') as outfile:
     for i in range(1,732):
-        string = "https://superheroapi.com/api.php/4056156634452255/" + str(i)
+        string = "https://superheroapi.com/api.php/access_token/" + str(i)
         response = requests.get(string)
         jsondata = response.json()
         json.dump(jsondata, outfile, indent=4)
